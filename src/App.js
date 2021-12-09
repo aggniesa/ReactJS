8e41457900ef92c352f90b8133a08af9ea9afa70
Ines MS <inemenasaravia@gmail.com>
2021-12-09 00:54:00 -0300

    Desafío: Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
 //Componentes
 import Navbar from "./Componentes/navbar/Navbar";
+import ItemListContainer from "./Componentes/ItemListContainer";
 
-/* function App() {
-  return (
-    <>
-    <div className="App">
-      <header className="App-header">
-      <h1> Tienda de productos </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
-  );
-} */
 
 function App () {
+
+  const links= [
+    {href:"#", name:"Inicio"},
+    {href:"#", name:"Productos"},
+    {href:"#", name:"Carrito"},
+
+  ]
+
   return (
     <>
-      <Navbar/>
+      <Navbar nombre="Ines" edad={29} links={links}/>
       <main>Main</main>
+      <ItemListContainer saludo=""/>
     </>
   );
 }
--- src/Componentes/navbar/Navbar.js
+++ src/Componentes/navbar/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
-import "./navbar.css";
+import "./navbar.scss";
 
-const Navbar = ( ) => {
+const Navbar = (props) => {
+    //console.log(props);
+    //console.log (props.nombre);
+
+    const {nombre,links} = props;
+
     return (
-        <header>
+        <header id="main-header" className="header">
         <>
-            <h1 className="header-h1">Tienda de productos</h1>
-            <span className="material-icons">
-            shopping_cart
-            </span>
-            <img src="../logo192.png" alt="logo"/>
+                <img src="../logo192.png" alt="logo"/>
+                <h1 className="header-h1">¡Bienvenidx, {nombre}!</h1>
             <nav>
-                <ul>
-                    <li><a href="#">link 1</a></li>
-                    <li><a href="#">link 2</a></li>
                 
-                </ul>
+                {links.map((elemento,indice)=>{
+                    return <a key={elemento.name} href={elemento.href}>{elemento.name}</a>
+                })} 
+                <span className="material-icons">
+                    shopping_cart
+                </span>
             </nav>
         </>
         </header>
